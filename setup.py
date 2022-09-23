db483f4af0c67b660bde6688dcf428c6c1c0437c
Joaquín Entrialgo <joaquin@uniovi.es>
2022-09-23 13:06:53 +0200

    Add missing pakcage. Black formatter also run on setup.py

--- setup.py
+++ setup.py
@@ -4,52 +4,58 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 
-    'malloovia',
-    'simpy',
-    'numpy',
-    'pandas'
+requirements = [
+    "Click>=7.0",
+    "malloovia",
+    "simpy",
+    "numpy",
+    "pandas",
+    "click_config_file",
 ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="ASI Uniovi",
-    author_email='joaquin@uniovi.es',
-    python_requires='>=3.7',
+    author_email="joaquin@uniovi.es",
+    python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Simulate transactional systems running on cloud infrastructure",
     entry_points={
-        'console_scripts': [
-            'simlloovia=simlloovia.cli:simulate',
+        "console_scripts": [
+            "simlloovia=simlloovia.cli:simulate",
         ],
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='simlloovia',
-    name='simlloovia',
-    packages=find_packages(include=['simlloovia', 'simlloovia.*']),
+    keywords="simlloovia",
+    name="simlloovia",
+    packages=find_packages(include=["simlloovia", "simlloovia.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/asi-uniovi/simlloovia',
-    version='1.0.0',
+    url="https://github.com/asi-uniovi/simlloovia",
+    version="1.0.0",
     zip_safe=False,
 )
